27ebce07067ac065547b2776502f37b493c7d39a
Tal Hever-Chybowski <thchybowski@gmail.com>
2019-02-25 23:47:53 +0100

    Add input folder for csv files

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ An automated publishing tool.
 
 # Enviroment variables
 PT_PASSWORD, PT_USER, PT_SITE
+
+# Input folder
+All csv files are stored within input/. This folder is defined to be excluded from git in .gitignore.
--- publish.js
+++ publish.js
@@ -1,9 +1,20 @@
 require('chromedriver');
 require('dotenv').config();
+const csv = require('csvtojson');
+const path = require('path');
+
+const csvFilePath = path.resolve("input", process.argv[2]);
+ 
+
 
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
+
+// Load input file into jsonArray
+  const jsonArray = await csv().fromFile(csvFilePath);
+
+
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get(process.env.PT_SITE);
@@ -12,6 +23,10 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.findElement(By.css('input[name="login"]')).click();
     await driver.wait(until.elementLocated(By.className('newEvent')));
     await driver.findElement(By.className('newEvent')).click();
+      
+      
+      
+      
       
 //    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
 //    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
